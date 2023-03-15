d8d6f1f5587cc9f4facb3bcfbf7c3486fb297921
Erick Llerenas <dev.llerenas@gmail.com>
2023-03-15 00:40:57 -0600

    init profile card

--- src/views/Home/components/Contact.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Contact = () => {
-	return <section className="bg-red-400 h-100%">Contact</section>;
-};
-
-export default Contact;
---
+++ src/views/Home/components/ProfileCard/Contact.tsx
@@ -0,0 +1,7 @@
+//Aqui haz tu esa cosa
+
+const Contact = ()=>{
+    return <div>cosadsadsadas</div>
+}
+
+export default Contact;
