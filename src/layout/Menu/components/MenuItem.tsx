4934da9b61c6580c2745fe6bd99004a96aea581c
Marco <=>
2023-03-27 20:12:12 -0600

    menu-colors

--- src/layout/Menu/components/MenuItem.tsx
+++ src/layout/Menu/components/MenuItem.tsx
@@ -2,27 +2,25 @@ import React, { ReactNode } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 interface MenuItemProps {
-  path: string;
-  icon: ReactNode;
-  label: string;
+	path: string;
+	icon: ReactNode;
+	label: string;
 }
 
 const MenuItem = ({ path, icon, label }: MenuItemProps): JSX.Element => {
-  const { pathname } = useLocation();
+	const { pathname } = useLocation();
 
-  const SELECTED = "bg-gradient-to-r from-pink-500 to-orange-400 text-white";
-
-  return (
-    <Link
-      to={path}
-      className={`bg-gray-100 flex flex-col p-4 rounded-xl items-center text-gray-700 text-xs font-medium cursor-pointer hover:bg-gradient-to-r from-pink-500 to-orange-400 hover:text-white ${
-        path === pathname && SELECTED
-      }`}
-    >
-      {icon}
-      <span>{label}</span>
-    </Link>
-  );
+	return (
+		<Link
+			to={path}
+			className={`bg-gray-100 flex flex-col justify-center rounded-xl items-center text-gray-700 text-xs font-medium cursor-pointer my-hover w-16 h-16 ${
+				path === pathname && "my-background"
+			}`}
+		>
+			{icon}
+			<span>{label}</span>
+		</Link>
+	);
 };
 
 export default MenuItem;
